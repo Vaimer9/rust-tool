b3e3c8f858adb69695b7e8921396d26f2faacf36
NightRaven055 <nightraven055@gmail.com>
2021-11-08 22:31:21 +0000

    added system name for sysinfo

--- src/stuff/sysinfo.rs
+++ src/stuff/sysinfo.rs
@@ -2,11 +2,12 @@ use super::functions::{figlet,clear_screen,input};
 use sysinfo::{NetworkExt, NetworksExt, ProcessExt, System, SystemExt};
 
 pub fn sysinfo() {
-    let mut system = System::new_all();
-    system.refresh_all();
+    let mut sys = System::new_all();
+    sys.refresh_all();
     loop {
         clear_screen();
         figlet("Sysinfo");
+        println!(" 1. System name:  {:?}", sys.name());
         let inputsysinfo = input();
     }
 }
